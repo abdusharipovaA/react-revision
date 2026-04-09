d87c9ec681d4f584b6866c9a0587eb1f414217c9
Abdusharipova A <abdwrpvvagmail.com>
2026-04-09 17:47:54 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { Routes, Route, Link } from "react-router";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Stopwatch from "./pages/Stopwatch";
+import Layout from "./layouts/layout";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/projects/stopwatch" element={<Stopwatch />}></Route>
+          <Route path="/projects/stopwatch" element={<Stopwatch />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
