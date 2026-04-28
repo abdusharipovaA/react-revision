ef31d02641b4275c7e7ae1d033a9a24bf8c3d64c
Abdusharipova A <abdwrpvvagmail.com>
2026-04-28 16:50:08 +0500

    fgnfjg

---
+++ src/pages/TodoApp.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router";
+
+function TodoApp() {
+  return (
+    <>
+      <h1>2. TodoApp</h1>
+      <div className="input">
+        <input type="text" placeholder="Add a task" />
+        <button>Add</button>
+      </div>
+      <div className="Board">
+        <div className="BoardBox">
+          <h1>ali ruzmetov</h1>
+          <button>Delete</button>
+        </div>
+        <div className="BoardBox">
+          <h1>ali ruzmetov</h1>
+          <button>Delete</button>
+        </div>
+        <div className="BoardBox">
+          <h1>ali ruzmetov</h1>
+          <button>Delete</button>
+        </div>
+        <div className="BoardBox">
+          <h1>ali ruzmetov</h1>
+          <button>Delete</button>
+        </div>
+        <div className="BoardBox">
+          <h1>ali ruzmetov</h1>
+          <button>Delete</button>
+        </div>
+        <div className="BoardBox">
+          <h1>ali ruzmetov</h1>
+          <button>Delete</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TodoApp;
